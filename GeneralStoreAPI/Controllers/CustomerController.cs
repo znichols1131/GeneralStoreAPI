fb66c4f94526ef12b4d1e4fcea9549ba11723577
Zach Nichols <znichols1131@gmail.com>
2021-11-03 10:35:58 -0400

    Finished POST GET PUT DELETE endpoints for Customer and Product. Tested in Postman.

--- GeneralStoreAPI/Controllers/CustomerController.cs
+++ GeneralStoreAPI/Controllers/CustomerController.cs
@@ -16,6 +16,7 @@ namespace GeneralStoreAPI.Controllers
 
         // POST
         // api/Customer
+        [HttpPost]
         public async Task<IHttpActionResult> PostCustomer([FromBody] Customer model)
         {
             if(model is null)
@@ -34,12 +35,66 @@ namespace GeneralStoreAPI.Controllers
 
         // GET ALL
         // api/Customer
+        [HttpGet]
         public async Task<IHttpActionResult> GetAll()
         {
             List<Customer> customers = await _context.Customers.ToListAsync();
             return Ok(customers);
         }
 
+        // GET BY ID
+        // api/Customer/{id}
+        [HttpGet]
+        public async Task<IHttpActionResult> GetById([FromUri] int id)
+        {
+            Customer customer = await _context.Customers.FindAsync(id);
 
+            if (customer is null)
+                return NotFound();
+
+            return Ok(customer);
+        }
+
+        // PUT
+        // api/Customer/{id}
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateById([FromUri] int id, [FromBody] Customer updatedCustomer)
+        {
+            if (id != updatedCustomer.Id)
+                return BadRequest("Ids do not match");
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            Customer originalCustomer = await _context.Customers.FindAsync(id);
+
+            if (originalCustomer is null)
+                return NotFound();
+
+            originalCustomer.FirstName = updatedCustomer.FirstName;
+            originalCustomer.LastName = updatedCustomer.LastName;
+
+            await _context.SaveChangesAsync();
+
+            return Ok($"Customer {originalCustomer.FullName} was updated.");
+        }
+
+        // DELETE
+        // api/Customer/{id}
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteCustomer([FromUri] int id)
+        {
+            Customer customer = await _context.Customers.FindAsync(id);
+
+            if (customer is null)
+                return NotFound();
+
+            _context.Customers.Remove(customer);
+
+            if (await _context.SaveChangesAsync() == 1)
+                return Ok("Customer was deleted.");
+
+            return InternalServerError();
+        }
     }
 }
--- GeneralStoreAPI/Controllers/ProductController.cs
+++ GeneralStoreAPI/Controllers/ProductController.cs
@@ -1,13 +1,101 @@
-﻿using System;
+﻿using GeneralStoreAPI.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace GeneralStoreAPI.Controllers
 {
     public class ProductController : ApiController
     {
+        private readonly GeneralStoreDbContext _context = new GeneralStoreDbContext();
+
+        // POST
+        // api/Product
+        [HttpPost]
+        public async Task<IHttpActionResult> PostProduct([FromBody] Product model)
+        {
+            if (model is null)
+                return BadRequest("Your request body cannot be empty");
+
+            if (ModelState.IsValid)
+            {
+                // Store the model in the database
+                _context.Products.Add(model);
+                await _context.SaveChangesAsync();
+                return Ok("Product was created.");
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        // GET ALL
+        // api/Product
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Product> products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        // GET BY ID
+        // api/Product/{id}
+        [HttpGet]
+        public async Task<IHttpActionResult> GetBySKU([FromUri] int id)
+        {
+            Product product = await _context.Products.FindAsync(id);
+
+            if (product is null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        // PUT
+        // api/Product/{id}
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateBySKU([FromUri] int id, [FromBody] Product updatedProduct)
+        {
+            if (id != updatedProduct.SKU)
+                return BadRequest("SKUs do not match");
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            Product originalProduct = await _context.Products.FindAsync(id);
+
+            if (originalProduct is null)
+                return NotFound();
+
+            originalProduct.Name = updatedProduct.Name;
+            originalProduct.Cost = updatedProduct.Cost;
+            originalProduct.NumberInInventory = updatedProduct.NumberInInventory;
+
+            await _context.SaveChangesAsync();
+
+            return Ok($"Product {originalProduct.Name} was updated.");
+        }
+
+        // DELETE
+        // api/Product/{id}
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteProduct([FromUri] int id)
+        {
+            Product product = await _context.Products.FindAsync(id);
+
+            if (product is null)
+                return NotFound();
+
+            _context.Products.Remove(product);
+
+            if (await _context.SaveChangesAsync() == 1)
+                return Ok("Product was deleted.");
+
+            return InternalServerError();
+        }
     }
 }
