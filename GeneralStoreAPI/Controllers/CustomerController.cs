91d56d8f801da6f8933edd4348283911400e672e
Zach Nichols <znichols1131@gmail.com>
2021-11-03 10:02:38 -0400

    Created POST and GETALL endpoints for Customer

--- GeneralStoreAPI/Controllers/CustomerController.cs
+++ GeneralStoreAPI/Controllers/CustomerController.cs
@@ -1,13 +1,45 @@
-﻿using System;
+﻿using GeneralStoreAPI.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace GeneralStoreAPI.Controllers
 {
     public class CustomerController : ApiController
     {
+        private readonly GeneralStoreDbContext _context = new GeneralStoreDbContext();
+
+        // POST
+        // api/Customer
+        public async Task<IHttpActionResult> PostCustomer([FromBody] Customer model)
+        {
+            if(model is null)
+                return BadRequest("Your request body cannot be empty");
+            
+            if(ModelState.IsValid)
+            {
+                // Store the model in the database
+                _context.Customers.Add(model);
+                await _context.SaveChangesAsync();
+                return Ok("Customer was created.");
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        // GET ALL
+        // api/Customer
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Customer> customers = await _context.Customers.ToListAsync();
+            return Ok(customers);
+        }
+
+
     }
 }
---
+++ GeneralStoreAPI/Models/GeneralStoreDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class GeneralStoreDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public GeneralStoreDbContext() : base("DefaultConnection") { }
+
+    }
+}
